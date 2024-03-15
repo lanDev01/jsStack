744e3691b3d9888ea6579066a6ea06a1012d8072
LanDev01 <alnjr0001@outlook.com>
2024-03-15 19:03:44 -0300

    feat: separando em pastas os arquivos e aplicando parâmetros padrão

---
+++ functions-and-arrowFunction/index.js
@@ -0,0 +1,19 @@
+// Short syntax
+
+// const num = 10;
+// const sum = () => (
+//         num >= 10 
+//             ? 'Maior igual a 10' 
+//             : 'Menor que 10'
+//     );
+
+// console.log(sum())
+
+const num = 10;
+
+const getUser = () => ({
+    id: '123',
+    name: 'Alan'
+})
+
+console.log(getUser())
